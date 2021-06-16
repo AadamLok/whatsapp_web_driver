10b0abed617adc1bc60882b6b0f86eb589292588
Aadam Lokhandwala <AADAMLOKHANDWALA786@GMAIL.COM>
2021-06-16 05:45:19 -0400

    Completed the todo for get and set status method

--- whatsapp_web_driver/custom_wait.py
+++ whatsapp_web_driver/custom_wait.py
@@ -1,5 +1,6 @@
 class presence_of_any_one_element_located(object):
-  """An expectation for checking that an element from the 2 element passed in has present
+  """
+  An expectation for checking that an element from the 2 element passed in has present
   """
   def __init__(self, locator1, locator2):
     self.locator1 = locator1
@@ -20,3 +21,17 @@ class presence_of_any_one_element_located(object):
         found_elem_2 = False
 
     return found_elem_1 or found_elem_2
+
+class element_has_some_text(object):
+  """
+  An expectation for checking that an element has some text loaded inside it
+  """
+  def __init__(self, locator):
+    self.locator = locator
+
+  def __call__(self, driver):
+    element = driver.find_element(*self.locator)
+    if element.text != "":
+        return element
+    else:
+        return False
--- whatsapp_web_driver/whatsapp_web_driver.py
+++ whatsapp_web_driver/whatsapp_web_driver.py
@@ -1,16 +1,17 @@
-from posixpath import expanduser
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 import time
 import os
+import win32clipboard
 
 from .custom_errors import ChromeDriverNotWorking, WhatsappNotLoggedIn, MaxTimeOut
-from .custom_wait import presence_of_any_one_element_located
+from .custom_wait import presence_of_any_one_element_located, element_has_some_text
 
 class WhatsappWebDriver():
     def __init__(self, chrome_driver="", max_wait=10):
@@ -42,18 +43,87 @@ class WhatsappWebDriver():
         except NoSuchElementException:
             return True
 
-    def change_status(self, new_status="Whatsapp being controlled by WhatsappWebDriver."):
-        #TODO check if whatsapp is logged in
-        #then change the status
-        #raise relevant errors
-        # in the end retun True
-        return True
+    def reload_page(self):
+        self.driver.get("https://web.whatsapp.com/")
 
-    def get_current_status(self):
-        #TODO get the current status
-        # and retun it
-        # raise relevant errors
-        return None
+    def open_profile_page(self):
+        self.reload_page()
+        
+        if not self.is_logged_in():
+            raise WhatsappNotLoggedIn()
+
+        side_page_profile_pic_element = None
+        side_page_profile_pic_element_XPATH = """//*[@id="side"]/header/div[1]/div/img"""
+        try:
+            side_page_profile_pic_element = WebDriverWait(self.driver, self.max_wait).until(
+                EC.element_to_be_clickable((By.XPATH,side_page_profile_pic_element_XPATH))
+            )
+        except TimeoutException:
+            raise MaxTimeOut()
+        
+        side_page_profile_pic_element.click()
+
+    def set_status(self, new_status="Whatsapp being controlled by WhatsappWebDriver."):
+        self.open_profile_page()
+        
+        edit_status_element = None
+        edit_status_element_XPATH = """//*[@id="app"]/div[1]/div[1]/div[2]/div[1]/span/div[1]/div/div/div[4]/div[2]/div[1]/span[2]/div/span"""
+        done_element = None
+        done_XPATH = """//*[@id="app"]/div[1]/div[1]/div[2]/div[1]/span/div[1]/div/div/div[4]/div[2]/div[1]/span[2]/div/span"""
+        try:
+            edit_status_element = WebDriverWait(self.driver, self.max_wait).until(
+                EC.element_to_be_clickable((By.XPATH,edit_status_element_XPATH))
+            )
+            edit_status_element.click()
+
+            win32clipboard.OpenClipboard()
+            win32clipboard.EmptyClipboard()
+            win32clipboard.SetClipboardText(new_status, win32clipboard.CF_UNICODETEXT)
+            win32clipboard.CloseClipboard()
+
+            action = ActionChains(self.driver)
+            action.key_down(Keys.SHIFT)
+            for i in range(140):
+                action.send_keys(Keys.LEFT)
+            action.key_up(Keys.SHIFT).send_keys(Keys.DELETE)
+            action.key_down(Keys.CONTROL).send_keys("v").key_up(Keys.CONTROL).perform()
+
+            done_element = WebDriverWait(self.driver, self.max_wait).until(
+                EC.element_to_be_clickable((By.XPATH, done_XPATH))
+            )
+            done_element.click()
+        except TimeoutException:
+            raise MaxTimeOut()
+        
+        return True
+    
+    def get_status(self):
+        self.open_profile_page()
+        
+        status_element_XPATH = """//*[@id="app"]/div[1]/div[1]/div[2]/div[1]/span/div[1]/div/div/div[4]/div[2]/div[1]/div/div[2]"""
+        status_edit_element = None
+        status_edit_element_XPATH = """//*[@id="app"]/div[1]/div[1]/div[2]/div[1]/span/div[1]/div/div/div[4]/div[2]/div[1]/span[2]/div/span"""
+        try:
+            WebDriverWait(self.driver, self.max_wait).until(
+                element_has_some_text((By.XPATH,status_element_XPATH))
+            )
+            status_edit_element = WebDriverWait(self.driver, self.max_wait).until(
+                EC.element_to_be_clickable((By.XPATH, status_edit_element_XPATH))
+            )
+            status_edit_element.click()
+        except TimeoutException:
+            raise MaxTimeOut()
+
+        action = ActionChains(self.driver).key_down(Keys.SHIFT)
+        for i in range(140):
+            action.send_keys(Keys.LEFT)
+        action.key_up(Keys.SHIFT).key_down(Keys.CONTROL).send_keys("C").key_up(Keys.CONTROL).perform()
+
+        win32clipboard.OpenClipboard()
+        status = win32clipboard.GetClipboardData(win32clipboard.CF_UNICODETEXT)
+        win32clipboard.CloseClipboard()
+
+        return status
 
     def close(self):
         self.driver.quit()
